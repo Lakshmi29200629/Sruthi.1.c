ee7eb794a49824916092eb4d490e82e147d0ff3f
Lakshmi29200629 <lakshmiraya2006@gmail.com>
2024-11-28 09:01:34 +0530

    Create Arrays in c(1D).c

---
+++ Arrays in c(1D).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  printf("Lakshmi.PS\n");
+  int numbers[5]={10,20,30,40,50};
+  printf("elements of the array;\n");
+  for(int i=0;i<5;i++){
+    printf("numbers[%d]=%d\n",i,numbers[i]);
+  }
+  return 0;
+}
+
+  
+    
