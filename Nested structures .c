88ed1253f888dad0af35223d54bb515251e8f2d3
Lakshmi29200629 <lakshmiraya2006@gmail.com>
2024-11-29 12:15:42 +0530

    Create Nested structures .c

---
+++ Nested structures .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+struct address{
+char city[50];
+int pin;
+};
+struct person{
+char name[50];
+struct address addr;
+};
+int main(){
+  struct person p;
+  printf("Enter name:");
+  scanf("%s",p.name);
+  printf("Enter city:");
+  scanf("%s",p.addr.city);
+  printf("Enter pin:");
+  scanf("%d",&p.addr.pin);
+printf("name:%s\ncity:%s\npin:%d\n",p.name,p.addr.city,p.addr.pin);
+  return 0;
+}
+
