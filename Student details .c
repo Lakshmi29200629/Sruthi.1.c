f2fef92968ce2ab84cff6a6cc57c690cb0a956e7
Lakshmi29200629 <lakshmiraya2006@gmail.com>
2024-11-29 11:06:57 +0530

    Create Student details .c

---
+++ Student details .c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+struct student{
+int age,gender,college_name,branch;
+char name[50];
+float college;
+};
+int main(){
+  struct student s1;
+  printf("Enter name:");
+  scanf(""%s",s1.name);
+  printf("Enter the age:");
+  scanf("%s",s1.age);
+  printf("Enter the gender:");
+  scanf("%s",%s1.gender:);
+  printf("Enter the college name:");
+  scanf("%s",%s1.college name:");
+  printf("Enter the branch name:");
+  scanf(""%s",%1.branch name:");
+  printf("\nstudent details:\n");
+printf("name:%s\nage:%d\ngender:%s\ncollege name:%s\nbranch",s1.name,s1.age,s1.gender.s1.college name,s1.branch name);
+  return 0;
+}
+  
+   
