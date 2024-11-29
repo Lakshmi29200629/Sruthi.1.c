d5d2a97f9daed8e0fd2bd43b72a742741dd5c26f
Lakshmi29200629 <lakshmiraya2006@gmail.com>
2024-11-29 22:13:00 +0530

    Create Program to calculate factorial using a function .c

---
+++ Program to calculate factorial using a function .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+    if (n == 0){
+        return 1;
+    }
+    return n * factorial(n - 1); 
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    } else {
+        printf("Factorial=%d\n",factorial(num));
+  return 0;
+}
